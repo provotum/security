ae60081de61e823348364c4f50090093d5a41977
Raphael Matile <raphael.matile@gmail.com>
2017-10-09 20:19:12 +0200

    Add Polynomial and corresponding test

--- src/main/java/org/provotum/security/arithmetic/Polynomial.java
+++ src/main/java/org/provotum/security/arithmetic/Polynomial.java
@@ -1,4 +1,72 @@
 package org.provotum.security.arithmetic;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Polynomial {
+
+    private ModInteger p;
+    private ModInteger q;
+    private ModInteger g;
+
+    private int degree;
+    private List<ModInteger> coefficients;
+
+    public Polynomial(ModInteger p, ModInteger q, ModInteger g, int degree) {
+        this.p = p;
+        this.q = q;
+        this.g = g;
+        this.degree = degree;
+
+        this.coefficients = new ArrayList<>();
+
+        for (int i = 0; i <= this.degree; i++) {
+            coefficients.add(ModInteger.random(this.q));
+        }
+    }
+
+    public int getDegree() {
+        return degree;
+    }
+
+    public List<ModInteger> getCoefficients() {
+        return coefficients;
+    }
+
+    public ModInteger evaluate(ModInteger x) {
+        ModInteger sum = new ModInteger(ModInteger.ZERO, this.q);
+
+        for (int i = 0; i < this.coefficients.size(); i++) {
+            ModInteger coefficient = this.coefficients.get(i);
+            sum = sum.add(coefficient.multiply(new ModInteger(x, this.q).pow(i)));
+        }
+
+        return sum;
+    }
+
+    public List<ModInteger> getLangrangeCoefficients() {
+        List<ModInteger> lagrangeCoefficients = new ArrayList<>(this.coefficients.size());
+
+        for (ModInteger next : this.coefficients) {
+            ModInteger numerator = new ModInteger(ModInteger.ONE, this.q);
+
+            for (ModInteger innerNext : this.coefficients) {
+                if (!next.equals(innerNext)) {
+                    numerator = numerator.multiply(this.q.subtract(innerNext));
+                }
+            }
+
+            ModInteger denominator = new ModInteger(ModInteger.ONE, this.q);
+
+            for (ModInteger innerNext : this.coefficients) {
+                if (!next.equals(innerNext)) {
+                    denominator = denominator.multiply(next.subtract(innerNext));
+                }
+            }
+
+            lagrangeCoefficients.add(numerator.divide(denominator));
+        }
+
+        return lagrangeCoefficients;
+    }
 }
